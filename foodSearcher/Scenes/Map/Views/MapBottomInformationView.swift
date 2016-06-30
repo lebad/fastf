b4c0878ff2b9b78723bc050297e7a62debcd15c7
andrey <lebedac@gmail.com>
2016-06-30 23:49:43 +0300

    map bottom inf view begins

---
+++ foodSearcher/Scenes/Map/Views/MapBottomInformationView.swift
@@ -0,0 +1,17 @@
+//
+//  MapBottomInformationView.swift
+//  foodSearcher
+//
+//  Created by andrey on 30/06/16.
+//  Copyright © 2016 AndreyLebedev. All rights reserved.
+//
+
+import UIKit
+
+class MapBottomInformationView: UIView {
+  
+  @IBOutlet weak var logoImage: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var addressLabel: UILabel!
+}
+
